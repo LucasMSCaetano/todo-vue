91d94a876df7804848a0e9fc8a4722050d390ba3
Lucas Matheus da Silva Caetano <lucas.caetano@presidencia.gov.br>
2022-09-12 15:03:35 -0300

    refactor: change default colors and adjustments on navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
     <nav >
         <v-toolbar flat app>
+            <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">Todo</span>
                 <span >Ninja</span>
@@ -9,12 +10,20 @@
             <span >Sing Out</span>
             <v-icon right>mdi-exit-to-app</v-icon>
         </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app class="primary">
+            <p>test</p>
+        </v-navigation-drawer>
     </nav>
 </template>
 
 <script>
 
 export default {
-
+    data(){
+        return{
+            drawer: false
+        }
+    }
 }
 </script>
