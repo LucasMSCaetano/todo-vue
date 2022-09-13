62e4d6cf2f5a24179ef743a51f29581236581d0b
Lucas Matheus da Silva Caetano <lucas.caetano@presidencia.gov.br>
2022-09-13 16:49:37 -0300

    feat: add expasion panel on projects

--- src/Views/ProjectsView.vue
+++ src/Views/ProjectsView.vue
@@ -1,21 +1,44 @@
 <template>
-    <div class="projects">
-        <h1 class="subheading grey--text">Projects</h1>
-        <v-container class="my-5">
-            <v-card flat class="pa-3">
-                <v-layout row wrap>
-                    <v-flex xs12 md6>
-                        <div class="caption grey--text" >project title</div>
-                        <div>Create a new website</div>
-                    </v-flex>
-                </v-layout>
-            </v-card>
-        </v-container>
-    </div>
+  <div class="projects">
+    <h1 class="subtitle-1 grey--text">Projects</h1>
+
+    <v-container class="my-5">
+      <v-expansion-panels>
+        <v-expansion-panel class="px-4" v-for="project in myProjects" :key="project.title">
+          <v-expansion-panel-header>
+            {{ project.title }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content class="grey--text">
+            <div class="font-weight-bold">due by {{ project.due }}</div>
+            <div>{{ project.content }}</div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-container>
+
+  </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  name: 'ProjectsView',
+  data() {
+    return {
+      projects: [
+        {title: 'design a new site', person:'The Net Ninja', due: '1st jan', status:'ongoing', content: 'aaaa'},
+                {title: 'code', person:'Chun Li', due: '1st jan', status:'complete', content: 'aaaa'},
+                {title: 'design videp', person:'Ryu', due: '1st jan', status:'complete', content: 'aaaa'},
+                {title: 'create a community', person:'Gouken', due: '1st jan', status:'overdue', content: 'aaaa'}
+      ]
+    }
+  },
+  computed: {
+    myProjects() {
+      return this.projects.filter(project => {
+        return project.person === 'The Net Ninja'
+      })
+    }
+  }
 }
 </script>
