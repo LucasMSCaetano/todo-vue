c6a2ec2fb7d84291d7bdb77ae01ad44b6d280a26
Lucas Matheus da Silva Caetano <lucas.caetano@presidencia.gov.br>
2022-09-13 16:30:17 -0300

    feat: add images on team view and on navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,6 +12,16 @@
         </v-toolbar>
 
         <v-navigation-drawer v-model="drawer" app class="primary">
+            <v-layout column align-center>
+                <v-flex class="mt-5">
+                    <v-avatar size="100">
+                        <img src="/avatar-1.png" alt="">
+                    </v-avatar>
+                    <p class="white--text subheading mt-1">
+                    The Net Ninja
+                    </p>
+                </v-flex>
+            </v-layout>
             <v-list>
                 <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-item-action>
